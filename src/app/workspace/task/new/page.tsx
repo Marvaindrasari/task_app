e976f77e50a6bb899f070da0c0c476f1b00fb4d1
unknown <marvaindrasari@gmail.com>
2026-05-14 20:11:59 +0700

    fix create new task page error

--- src/app/workspace/task/new/page.tsx
+++ src/app/workspace/task/new/page.tsx
@@ -1,21 +1,19 @@
-// ...existing code...
 "use client";
 
 import { useParams } from "next/navigation";
 import CreateNewTask from "@/features/task/components/create-new-task";
 
 export default function NewProjectPage() {
-    const params = useParams();              // ubah sini
-    const workspaceId = params.workspaceId as string; // ambil workspaceId dari params
-    const projectId = params.projectId as string; // ambil workspaceId dari params
+    const params = useParams();
+    const workspaceId = params.workspaceId as string;
 
     if (!workspaceId) {
-        return <div className="p-6">Loading...</div>; // atau return null / notFound()
+        return <div className="p-6">Loading...</div>;
     }
 
     return (
         <div className="p-6">
-            <CreateNewTask workspaceId={workspaceId}/>
+            <CreateNewTask workspaceId={workspaceId} />
         </div>
     );
 }
