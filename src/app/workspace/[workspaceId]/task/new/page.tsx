be5839249fe8e4516e5790ca94b1d79168d3d2e1
unknown <marvaindrasari@gmail.com>
2026-05-14 20:35:52 +0700

    fix create new task final

--- src/app/workspace/[workspaceId]/task/new/page.tsx
+++ src/app/workspace/[workspaceId]/task/new/page.tsx
@@ -7,7 +7,7 @@ import CreateNewTask from "@/features/task/components/create-new-task";
 export default function NewProjectPage() {
     const params = useParams();              // ubah sini
     const workspaceId = params.workspaceId as string; // ambil workspaceId dari params
-    const projectId = params.projectId as string; // ambil workspaceId dari params
+    //const projectId = params.projectId as string; // ambil workspaceId dari params
 
     if (!workspaceId) {
         return <div className="p-6">Loading...</div>; // atau return null / notFound()
@@ -15,7 +15,7 @@ export default function NewProjectPage() {
 
     return (
         <div className="p-6">
-            <CreateNewTask workspaceId={workspaceId} projectId={projectId}/>
+            <CreateNewTask workspaceId={workspaceId}/>
         </div>
     );
 }
