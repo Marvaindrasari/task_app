51e1b552a2102624c158d1983a204c9a77f7025d
unknown <marvaindrasari@gmail.com>
2026-05-15 20:26:41 +0700

    fix project env

--- src/lib/appwrite-server.ts
+++ src/lib/appwrite-server.ts
@@ -4,7 +4,7 @@ import { Client, Databases, Account, Users } from "node-appwrite";
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
   .setKey(process.env.NEXT_APPWRITE_KEY!); // server key
 
 export const databases = new Databases(client);
